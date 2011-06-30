81955c2e1a9c39c8cac59f0634d0401b6cfebe3d
Marko Harjula <marko.harjula@nokia.com>
2011-06-30 09:50:26 +0300

    fix warnings

--- src/lan/sahara/webirc/server/ClientIrcObject.java
+++ src/lan/sahara/webirc/server/ClientIrcObject.java
@@ -10,7 +10,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.TreeMap;
 import lan.sahara.webirc.shared.IrcEntry;
 import org.jibble.pircbot.*;

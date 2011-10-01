3ac1fee08a426b0ed332917548d0afbdcf018cc8
Marko Harjula <marko.harjula@gmail.com>
2011-10-01 13:49:58 +0300

    add missing file

---
+++ src/lan/sahara/webirc/shared/IrcEntryTypes.java
@@ -0,0 +1,8 @@
+package lan.sahara.webirc.shared;
+
+public class IrcEntryTypes {
+	public static Integer E_MSG = 0;
+	public static Integer E_JOIN = 1;
+	public static Integer E_PART = 2;
+	public static Integer E_QUIT = 3;
+}
